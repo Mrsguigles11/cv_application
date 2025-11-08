19b4026b29180f3738513b422256e5544c42b123
Mrsguigles11 <harry_woodgate11@hotmail.co.uk>
2025-11-08 18:56:07 +0000

    add a transition to collapse the input section

--- src/components/input_section.jsx
+++ src/components/input_section.jsx
@@ -1,38 +1,43 @@
 import "../styles/input_section.css";
 
+function handleClick () {
+  const collapsibleContent = document.querySelector('.collapsible_content');
+  const inputSection = document.querySelector('.input_section');
+  collapsibleContent.className === "collapsible_content hidden" ? collapsibleContent.className = "collapsible_content visible" : collapsibleContent.className = "collapsible_content hidden";
+  inputSection.className === "input_section" ? inputSection.className = "input_section expanded" : inputSection.className = "input_section";
+}
+
 function InputSection({ heading, inputs, onChange, content, symbol }) {
   return (
     <div className="input_section">
-      <div className="input_heading_container">
-      <h2>{heading}</h2>
-      {symbol}
+      <div className="input_heading_container" onClick={handleClick}>
+        <h2>{heading}</h2>
+        {symbol}
       </div>
-      {inputs.map((input) => {
-        if (input === "Profile") {
+      <div className="collapsible_content hidden">
+        {inputs.map((input) => {
           return (
             <div className="input" key={input}>
               <label htmlFor={input}>{input}</label>
-              <textarea
-                spellCheck={false}
-                name={input}
-                onChange={(e) => onChange(e.target.value, heading, input)}
-                value={content[heading][input]}
-              />
+              {input === "Profile" ? (
+                <textarea
+                  spellCheck={false}
+                  name={input}
+                  onChange={(e) => onChange(e.target.value, heading, input)}
+                  value={content[heading][input]}
+                />
+              ) : (
+                <input
+                  type="text"
+                  name={input}
+                  onChange={(e) => onChange(e.target.value, heading, input)}
+                  value={content[heading][input]}
+                />
+              )}
             </div>
           );
-        }
-        return (
-          <div className="input" key={input}>
-            <label htmlFor={input}>{input}</label>
-            <input
-              type="text"
-              name={input}
-              onChange={(e) => onChange(e.target.value, heading, input)}
-              value={content[heading][input]}
-            />
-          </div>
-        );
-      })}
+        })}
+      </div>
     </div>
   );
 }
