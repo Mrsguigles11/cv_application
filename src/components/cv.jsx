f9b94e6c45758c429bac008e36935c5005732b08
Mrsguigles11 <harry_woodgate11@hotmail.co.uk>
2025-11-09 14:56:01 +0000

    Add background hover to input section and resize slightly

--- src/components/cv.jsx
+++ src/components/cv.jsx
@@ -5,9 +5,9 @@ function CV({ text }) {
     <div className="cv">
       <h2>{text["General Information"]["Name"]}</h2>
       <div className="details_container">
-        <div><img src="../public/email-outline.svg" alt="email_symbol"/>{text["General Information"]["Email"]}</div>
-        <div><img src="../public/phone.svg" alt="email_symbol"/>{text["General Information"]["Phone Number"]}</div>
-        <div><img src="../public/github.svg" alt="email_symbol"/>{text["General Information"]["Github"]}</div>
+        <div><img src="/email-outline.svg" alt="email_symbol"/>{text["General Information"]["Email"]}</div>
+        <div><img src="/phone.svg" alt="email_symbol"/>{text["General Information"]["Phone Number"]}</div>
+        <div><img src="/github.svg" alt="email_symbol"/>{text["General Information"]["Github"]}</div>
       </div>
       <h3>Profile</h3>
       <div className="profile">{text["General Information"]["Profile"]}</div>
