b2e91846f591a57a28a5e4f1177d8be054d67300
Mrsguigles11 <harry_woodgate11@hotmail.co.uk>
2025-11-11 10:44:07 +0000

    commit to switch machines

--- src/components/input_edu_exp.jsx
+++ src/components/input_edu_exp.jsx
@@ -8,6 +8,7 @@ function InputEducationalExperience({ heading, inputs, onChange, content, headin
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>school</title><path d="M12,3L1,9L12,15L21,10.09V17H23V9M5,13.18V17.18L12,21L19,17.18V13.18L12,17L5,13.18Z" /></svg>
       </div>
       <div className="collapsible_content hidden">
+        <div className="education_input"></div>
         {inputs.map((input) => {
           return (
             <div className="input" key={input}>
